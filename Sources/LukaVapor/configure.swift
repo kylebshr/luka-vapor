33ffb42558de1bb5b45b6345159bce25d542f3fc
Kyle Bashour <kylebshr@me.com>
2025-12-14 20:51:24 -0800

    Banners

--- Sources/LukaVapor/configure.swift
+++ Sources/LukaVapor/configure.swift
@@ -9,45 +9,44 @@ public func configure(_ app: Application) async throws {
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
     // Configure APNS
-    guard let pemString = Environment.get("PUSH_NOTIFICATION_PEM"),
-          let keyID = Environment.get("PUSH_NOTIFICATION_ID"),
-          let teamID = Environment.get("TEAM_IDENTIFIER") else {
-        throw Abort(.internalServerError, reason: "Missing APNS environment variables")
+    if let pemString = Environment.get("PUSH_NOTIFICATION_PEM"),
+       let keyID = Environment.get("PUSH_NOTIFICATION_ID"),
+       let teamID = Environment.get("TEAM_IDENTIFIER") {
+
+        let apnsdev = APNSClientConfiguration(
+            authenticationMethod: .jwt(
+                privateKey: try .loadFrom(string: pemString),
+                keyIdentifier: keyID,
+                teamIdentifier: teamID
+            ),
+            environment: .development
+        )
+
+        let apnsprod = APNSClientConfiguration(
+            authenticationMethod: .jwt(
+                privateKey: try .loadFrom(string: pemString),
+                keyIdentifier: keyID,
+                teamIdentifier: teamID
+            ),
+            environment: .production
+        )
+
+        await app.apns.containers.use(
+            apnsdev,
+            eventLoopGroupProvider: .shared(app.eventLoopGroup),
+            responseDecoder: JSONDecoder(),
+            requestEncoder: JSONEncoder(),
+            as: .development
+        )
+
+        await app.apns.containers.use(
+            apnsprod,
+            eventLoopGroupProvider: .shared(app.eventLoopGroup),
+            responseDecoder: JSONDecoder(),
+            requestEncoder: JSONEncoder(),
+            as: .production
+        )
     }
-
-    let apnsdev = APNSClientConfiguration(
-        authenticationMethod: .jwt(
-            privateKey: try .loadFrom(string: pemString),
-            keyIdentifier: keyID,
-            teamIdentifier: teamID
-        ),
-        environment: .development
-    )
-
-    let apnsprod = APNSClientConfiguration(
-        authenticationMethod: .jwt(
-            privateKey: try .loadFrom(string: pemString),
-            keyIdentifier: keyID,
-            teamIdentifier: teamID
-        ),
-        environment: .production
-    )
-
-    await app.apns.containers.use(
-        apnsdev,
-        eventLoopGroupProvider: .shared(app.eventLoopGroup),
-        responseDecoder: JSONDecoder(),
-        requestEncoder: JSONEncoder(),
-        as: .development
-    )
-
-    await app.apns.containers.use(
-        apnsprod,
-        eventLoopGroupProvider: .shared(app.eventLoopGroup),
-        responseDecoder: JSONDecoder(),
-        requestEncoder: JSONEncoder(),
-        as: .production
-    )
 
     // register routes
     try routes(app)
