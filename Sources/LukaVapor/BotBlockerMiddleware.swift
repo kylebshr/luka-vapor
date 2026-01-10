eb5ffcc8e99080b7562819c8089185a93d1a68c1
Kyle Bashour <kylebshr@me.com>
2026-01-09 23:54:07 -0800

    Fix warning

--- Sources/LukaVapor/BotBlockerMiddleware.swift
+++ Sources/LukaVapor/BotBlockerMiddleware.swift
@@ -2,7 +2,7 @@ import Vapor
 
 /// Middleware that blocks common bot/scanner requests probing for PHP vulnerabilities
 struct BotBlockerMiddleware: AsyncMiddleware {
-    func respond(to request: Request, chainingTo next: AsyncResponder) async throws -> Response {
+    func respond(to request: Request, chainingTo next: any AsyncResponder) async throws -> Response {
         let path = request.url.path.lowercased()
 
         // Block all .php requests - this is a Swift app, there are no legitimate PHP endpoints
